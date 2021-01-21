ddfead5e61c8aa24029224fb67c5d93562f0900c
MiniDay <372403923@qq.com>
2021-01-21 21:41:24 +0800

    优化 StringReplacer

--- src/main/java/net/airgame/bukkit/api/data/StringArrayReplacer.java
+++ src/main/java/net/airgame/bukkit/api/data/StringArrayReplacer.java
@@ -1,14 +1,19 @@
 package net.airgame.bukkit.api.data;
 
-import net.airgame.bukkit.api.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 public class StringArrayReplacer {
     private final String[] strings;
+    private final ArrayList<Map.Entry<String, String>> replaces;
 
     public StringArrayReplacer(String[] strings) {
         this.strings = strings;
+        replaces = new ArrayList<>();
     }
 
     @NotNull
@@ -18,12 +23,19 @@ public class StringArrayReplacer {
 
     @NotNull
     public StringArrayReplacer replace(@NotNull String key, @NotNull String value) {
-        StringUtils.replaceStringList(strings, key, value);
+        replaces.add(new AbstractMap.SimpleEntry<>(key, value));
         return this;
     }
 
     @NotNull
     public String[] getStrings() {
+        for (int i = 0; i < strings.length; i++) {
+            String s = strings[i];
+            for (Map.Entry<String, String> entry : replaces) {
+                s = s.replace(entry.getKey(), entry.getValue());
+            }
+            strings[i] = s;
+        }
         return strings;
     }
 }
--- src/main/java/net/airgame/bukkit/api/data/StringListReplacer.java
+++ src/main/java/net/airgame/bukkit/api/data/StringListReplacer.java
@@ -1,16 +1,20 @@
 package net.airgame.bukkit.api.data;
 
-import net.airgame.bukkit.api.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 public class StringListReplacer {
     private final List<String> list;
+    private final ArrayList<Map.Entry<String, String>> replaces;
 
     public StringListReplacer(@NotNull List<String> list) {
         this.list = list;
+        replaces = new ArrayList<>();
     }
 
     @NotNull
@@ -20,12 +24,19 @@ public class StringListReplacer {
 
     @NotNull
     public StringListReplacer replace(@NotNull String key, @NotNull String value) {
-        StringUtils.replaceStringList(list, key, value);
+        replaces.add(new AbstractMap.SimpleEntry<>(key, value));
         return this;
     }
 
     @NotNull
     public List<String> getList() {
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            for (Map.Entry<String, String> entry : replaces) {
+                s = s.replace(entry.getKey(), entry.getValue());
+            }
+            list.set(i, s);
+        }
         return list;
     }
 }
