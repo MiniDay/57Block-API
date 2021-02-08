2487e4459b8e0a80cf91128d320e92f69cdc62f2
MiniDay <372403923@qq.com>
2021-02-08 21:29:53 +0800

    添加 JsonArrayBuilder

---
+++ src/main/java/net/airgame/bukkit/api/util/data/JsonArrayBuilder.java
@@ -0,0 +1,55 @@
+package net.airgame.bukkit.api.util.data;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+@SuppressWarnings("unused")
+public class JsonArrayBuilder {
+    private final JsonArray array;
+
+    public JsonArrayBuilder() {
+        this(new JsonArray());
+    }
+
+    public JsonArrayBuilder(JsonArray array) {
+        this.array = array;
+    }
+
+    public static JsonArrayBuilder create() {
+        return new JsonArrayBuilder();
+    }
+
+    public JsonArrayBuilder append(String value) {
+        array.add(value);
+        return this;
+    }
+
+    public JsonArrayBuilder append(Number value) {
+        array.add(value);
+        return this;
+    }
+
+    public JsonArrayBuilder append(Boolean value) {
+        array.add(value);
+        return this;
+    }
+
+    public JsonArrayBuilder append(Character value) {
+        array.add(value);
+        return this;
+    }
+
+    public JsonArrayBuilder append(JsonElement value) {
+        array.add(value);
+        return this;
+    }
+
+    public JsonArrayBuilder appendAll(JsonArray value) {
+        array.addAll(value);
+        return this;
+    }
+
+    public JsonArray build() {
+        return array;
+    }
+}
